9e843346d41db101306c4e9473b0ef4934badb71
Adhimulammanjunath <162984255+Adhimulammanjunath@users.noreply.github.com>
2024-03-13 09:34:24 +0530

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,14 @@
+import numpy as np
+n=input('enter the signal')
+print(n)
+red=0
+green=1
+yellow=2
+if n==red:
+    print('stop')
+elif n==green:
+    print('move')
+else:
+    print('yellow')
+
+
---
+++ untitled4.py
@@ -0,0 +1,17 @@
+import numpy as np
+def SI(p,t,r):
+p=input('enter the profit')
+print(p)
+t=input('enter the time')
+print(t)
+r=input('enter the rate of intrest')
+print(r)
+SI=p*t*r/100
+print(SI)
+def CI(p,t,r,n):
+n=input('no.of times intrest applied in year')
+print(n)
+CI=(1+r/n)^nt
+print(CI)
+
+
